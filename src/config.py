fe9054e5e1b5410ac1f43ffa915549a72a95bcb1
Marcus <73910894+Rezzorex@users.noreply.github.com>
2026-08-15 17:42:04 +0200

    Add default database url value

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
+    DATABASE_URL: str = "http://qdrant:6333"
     COLLECTION_NAME: str = "Steam-Style-Items"
     MODEL_NAME: str = "google/siglip2-base-patch16-224"
     IMAGE_EMBEDDING_DIM: int = 768
